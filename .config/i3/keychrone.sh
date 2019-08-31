0bb200a97708b8eb75946d5c4179576363421f1e
j2ko <j2ko.pro@gmail.com>
2019-08-31 22:10:41 +0300

    Added better names for modes. removed redundant code

--- .config/i3/keychrone.sh
+++ .config/i3/keychrone.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#TODO instead of setting layout to specific device set it for all input devices added.
 export DISPLAY=":0"
 export XAUTHORITY="/home/j2ko/.Xauthority"
 export HOME="/home/j2ko"
 
-daskb_id=`/usr/bin/xinput -list | grep "${HID_NAME}" | grep -o id=[0-9]. | grep -o [0-9]. | tail -1`
-
-echo $daskb_id >> /tmp/testenv
-# setxkbmap -device "${daskb_id}" -option 'grp:alt_shift_toggle' -layout "us,ua"
-setxkbmap -option 'grp:alt_shift_toggle' -layout "us,ua"
-
+#Fix for F row
 echo 0 | tee /sys/module/hid_apple/parameters/fnmode
-su j2ko -c 'notify-send "Connected ${HID_NAME}"'
+
+#Fix layout for newly connected keychrone device
+daskb_id=`/usr/bin/xinput -list | grep "${HID_NAME}" | grep -o id=[0-9]. | grep -o [0-9]. | tail -1`
+setxkbmap -device "${daskb_id}" -option 'grp:alt_shift_toggle' -layout "us,ua"
+
+#Show notification about connection established
+su j2ko -c 'notify-send "Connected ${DISPLAY_NAME}"'
