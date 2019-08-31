ad210f3233b0c4137dd545c78f312e9f702d4e61
j2ko <j2ko.pro@gmail.com>
2019-08-31 21:55:24 +0300

    Update rules for USB and bluetooth connection

--- .config/i3/keychrone.sh
+++ .config/i3/keychrone.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #TODO instead of setting layout to specific device set it for all input devices added.
-export DISPLAY=:0
+export DISPLAY=":0"
+export XAUTHORITY="/home/j2ko/.Xauthority"
+export HOME="/home/j2ko"
+
+daskb_id=`/usr/bin/xinput -list | grep "${HID_NAME}" | grep -o id=[0-9]. | grep -o [0-9]. | tail -1`
+
+echo $daskb_id >> /tmp/testenv
+# setxkbmap -device "${daskb_id}" -option 'grp:alt_shift_toggle' -layout "us,ua"
+setxkbmap -option 'grp:alt_shift_toggle' -layout "us,ua"
+
 echo 0 | tee /sys/module/hid_apple/parameters/fnmode
-daskb_id=`xinput -list | grep 'Keychron K2' | grep -o id=[0-9]. | grep -o [0-9]. | tail -1`
-setxkbmap -device "${daskb_id}" -option 'grp:alt_shift_toggle' -layout "us,ua"
-su j2ko -c 'notify-send Keychrone K2 connected.'
+su j2ko -c 'notify-send "Connected ${HID_NAME}"'
